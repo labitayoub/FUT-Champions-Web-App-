9cf6f01742c1c0ea89b83f579561e2a2792be444
labitayoub <ayoub.labite@gmail.com>
2024-11-27 15:28:22 +0100

    update

--- index.js
+++ index.js
@@ -6,19 +6,60 @@ let position = document.getElementById('position');
 let gkPart = document.getElementById('gkPart');
 let plPart = document.getElementById('plPart');
 
-upload.onchange = function() {
-    profil.src = URL.createObjectURL(upload.files[0]);
+let errorMsg = document.getElementById('errorMsg');
+
+upload.onchange = function () {
+   profil.src = URL.createObjectURL(upload.files[0]);
 }
 
 function afficher() {
 
-            let posiChoix = position.value;
+   let posiChoix = position.value;
 
-            if (posiChoix === 'GK') {
-               plPart.classList.add('hidden');
-                gkPart.classList.remove('hidden');
-            } else {
-               plPart.classList.remove('hidden');
-                gkPart.classList.add('hidden');
-            }
+   if (posiChoix === "Select the player") {
+      plPart.classList.add('hidden');
+      gkPart.classList.add('hidden');
+      positionValide.innerText = 'Select the player, please.';
+   }
+   else if (posiChoix === 'GK') {
+      plPart.classList.add('hidden');
+      gkPart.classList.remove('hidden');
+      positionValide.innerText = '';
+   }else {
+      plPart.classList.remove('hidden');
+      gkPart.classList.add('hidden');
+      positionValide.innerText = '';
+   }
+
 }
+function validation() {
+
+   let nameError = document.getElementById('name').value;
+   let nationalityError = document.getElementById('nationality').value;
+   let clubError = document.getElementById('club').value;
+
+   if (nameError === "") {
+
+      errorName.innerText = 'name is required';
+   }
+   else {
+      errorName.innerText = 'valid';
+
+   }
+   if (nationalityError === "") {
+      errorNationality.innerText = 'name is required';
+   }
+   else {
+      errorNationality.innerText = 'valid';
+
+   }
+   if (clubError === "") {
+      errorClub.innerText = 'name is required';
+   }
+   else {
+      errorClub.innerText = 'valid';
+
+   }
+
+
+};
